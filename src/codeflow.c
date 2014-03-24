88517953df82c42de0942c4bc273833d2fcfb7b2
João Pinho <jpe.pinho@gmail.com>
2014-03-24 16:06:09 +0000

    version compiling without warning (-ansi -pedantic -Wall), waiting for assignment specification...

--- src/codeflow.c
+++ src/codeflow.c
@@ -5,7 +5,7 @@
 
 #define oo 1 << 28
 
-//data types
+/* data types */
 typedef struct node	   { int value; struct node* prev; struct node* next; } node;
 typedef struct vertex  { int value; struct list* edges;					  } vertex;
 typedef struct vnode   { vertex* value; struct vnode* next;				  } vnode;
@@ -13,13 +13,13 @@ typedef struct list	   { node* head; node* tail; int length;			  } list;
 typedef struct vlist   { vnode* head; vnode* tail; int length;			  } vlist;
 typedef struct graph   { vertex** vertices;	int length; int partition[2]; } graph;
 
-//constants
+/* constants */
 static vertex* NIL;
 static int* Dist = NULL;
 static vertex** Pair = NULL;
 static vlist* Q = NULL;
 
-//prototypes
+/* prototypes */
 node* node_new();
 list* list_new();
 vnode* vnode_new();
@@ -36,61 +36,99 @@ int hopcroft_karp(graph*);
 int bfs(graph*);
 int dfs(graph*, vertex*);
 
-
-/**
- * Problem of Jobs Max Allocation
- * Implemented based on the HopCroft-Karp Algorithm.
- */
-int main(int argc, char* argv[])
-{
-	graph* jobs = NULL;
-	int nstud, noffers, stud_i, njobs, jobid, n;
-	float seconds;
-	clock_t start, end;		
-
-	start = clock();
+/* functions - queues */
+void enqueue(vlist* queue, vertex* vx){
+	vnode* vn = vnode_new();
+	vn->value = vx;
+    
+	if(queue->head==NULL)
+		queue->head = vn;
+	else
+		queue->tail->next = vn;
 	
-	//init special null vertex
-	NIL = vertex_new();
-	NIL->edges = NULL;
-	NIL->value = -1;
-
-	fscanf(stdin, "%d %d\n", &nstud, &noffers);
-
-	n = noffers + nstud;
-	jobs = graph_new(n);
-
-	for(stud_i=0; stud_i<nstud; stud_i++) {
-		fscanf(stdin, "%d", &njobs);
-
-		while(njobs-- > 0) {
-			fscanf(stdin,"%d", &jobid);
-			graph_add(jobs, noffers+stud_i, jobid);
-		}
-	}
-	
-	jobs->partition[0] = noffers; //{students}
-	jobs->partition[1] = 0;		  //{jobs}
-
-	printf("%d\n", hopcroft_karp(jobs));
-	
-	end = clock();
-	seconds = (float)(end - start) / (float)CLOCKS_PER_SEC;
-	
-	printf ("Your calculations took %.2lf seconds to run.\n", seconds );	
-	
-	return 0;
+	queue->tail = vn;
+	queue->length++;
 }
 
-//functions - max flows
+vertex* dequeue(vlist* queue){
+	vnode* root;
+	vertex* vx;
+    
+	if(!queue || !queue->head) return NULL;
+	
+	root = queue->head;
+	queue->head = queue->head->next;
+    
+	queue->length--;
+	vx = root->value;
+	return vx;
+}
+
+/* functions - node */
+node* node_new(){
+	node* n = (node*)malloc(sizeof(node));
+	n->prev = NULL;
+	n->next = NULL;
+	n->value=0;
+	return n;
+}
+
+vnode* vnode_new(){
+	vnode* n = (vnode*)malloc(sizeof(vnode));
+	n->next = NULL;
+	n->value = NULL;
+	return n;
+}
+
+/* functions - vertex */
+vertex* vertex_new(){
+	vertex* v = (vertex*)malloc(sizeof(vertex));
+	v->edges = NULL;
+	v->value = 0;
+	return v;
+}
+
+/* functions - list*/
+list* list_new(){
+	list* lst = (list*)malloc(sizeof(list));
+	lst->head = lst->tail = NULL;
+	lst->length=0;
+	return lst;
+}
+
+vlist* vlist_new(){
+	vlist* lst = (vlist*)malloc(sizeof(vlist));
+	lst->head = lst->tail = NULL;
+	lst->length=0;
+	return lst;
+}
+
+void list_add(list* lst, int value){
+	node* n = node_new();
+	n->value = value;
+	
+	if(lst->head == NULL)
+		lst->head = n;
+	else {
+		lst->tail->next = n;
+		n->prev = lst->tail;
+	}
+    
+	lst->tail = n;
+	lst->length++;
+}
+
+/* functions - max flows */
 int hopcroft_karp(graph* G){
 	int f=0, i;
 	vertex* v;
-	Dist = ((int*)malloc((1 + G->length)*sizeof(int))) + 1; //{ptr shift, so that [-1] is at first position}
+    
+    /* {ptr shift, so that [-1] is at first position} */
+	Dist = ((int*)malloc((1 + G->length)*sizeof(int))) + 1;
 	Pair = (vertex**)malloc(G->length*sizeof(vertex*));
 	Q = vlist_new();
 
-	//init Dist={oo} and Pair={NIL}
+	/* init Dist={oo} and Pair={NIL} */
 	for(i=0; i < (G->length + 1); Dist[i] = oo, Pair[i] = NIL, i++) ;
 
 	while(bfs(G))
@@ -161,89 +199,7 @@ int dfs(graph* G, vertex* v){
    return 1;
 }
 
-//functions - queues
-void enqueue(vlist* queue, vertex* vx){
-	vnode* vn = vnode_new();
-	vn->value = vx;
-
-	if(queue->head==NULL) 
-		queue->head = vn;	
-	else 
-		queue->tail->next = vn;
-	
-	queue->tail = vn;
-	queue->length++;	
-}
-
-vertex* dequeue(vlist* queue){
-	vnode* root;
-	vertex* vx;
-
-	if(!queue || !queue->head) return NULL;
-	
-	root = queue->head;
-	queue->head = queue->head->next;
-
-	queue->length--;
-	vx = root->value;
-	return vx;
-}
-
-//functions - node
-node* node_new(){
-	node* n = (node*)malloc(sizeof(node));
-	n->prev = NULL;
-	n->next = NULL;
-	n->value=0;
-	return n;
-}
-
-vnode* vnode_new(){
-	vnode* n = (vnode*)malloc(sizeof(vnode));
-	n->next = NULL;
-	n->value = NULL;
-	return n;
-}
-
-//functions - vertex
-vertex* vertex_new(){
-	vertex* v = (vertex*)malloc(sizeof(vertex));
-	v->edges = NULL;
-	v->value = 0;
-	return v;
-}
-
-//functions - list
-list* list_new(){
-	list* lst = (list*)malloc(sizeof(list));
-	lst->head = lst->tail = NULL;
-	lst->length=0;
-	return lst;
-}
-
-vlist* vlist_new(){
-	vlist* lst = (vlist*)malloc(sizeof(vlist));
-	lst->head = lst->tail = NULL;
-	lst->length=0;
-	return lst;
-}
-
-void list_add(list* lst, int value){
-	node* n = node_new();
-	n->value = value;
-	
-	if(lst->head == NULL)
-		lst->head = n;
-	else {
-		lst->tail->next = n;
-		n->prev = lst->tail;
-	}
-
-	lst->tail = n;
-	lst->length++;
-}
-
-//functions - graph
+/* functions - graph */
 graph* graph_new(int vertices){
 	graph* grp = (graph*)malloc(sizeof(graph));
 	grp->vertices = (vertex**)calloc(vertices, sizeof(vertex*));
@@ -255,7 +211,54 @@ void graph_add(graph* grp, int vertice, int edge){
 	if(grp->vertices[vertice] == NULL){
 		grp->vertices[vertice] = vertex_new();
 		grp->vertices[vertice]->value = vertice;
-		grp->vertices[vertice]->edges = list_new();	
+		grp->vertices[vertice]->edges = list_new();
 	}
 	list_add(grp->vertices[vertice]->edges, edge);
+}
+
+
+/**
+ * problem of jobs max allocation
+ * implemented based on the hopcroft-karp algorithm.
+ */
+int main(int argc, char* argv[])
+{
+	graph* jobs = NULL;
+	int nstud, noffers, stud_i, njobs, jobid, n;
+	float seconds;
+	clock_t start, end;
+    
+	start = clock();
+	
+	/* init special null vertex */
+	NIL = vertex_new();
+	NIL->edges = NULL;
+	NIL->value = -1;
+    
+	fscanf(stdin, "%d %d\n", &nstud, &noffers);
+    
+	n = noffers + nstud;
+	jobs = graph_new(n);
+    
+	for(stud_i=0; stud_i<nstud; stud_i++) {
+		fscanf(stdin, "%d", &njobs);
+        
+		while(njobs-- > 0) {
+			fscanf(stdin,"%d", &jobid);
+			graph_add(jobs, noffers+stud_i, jobid);
+		}
+	}
+	
+	jobs->partition[0] = noffers; /* {students} */
+	jobs->partition[1] = 0;		  /* {jobs} */
+    
+	printf("%d\n", hopcroft_karp(jobs));
+	
+	end = clock();
+	seconds = (float)(end - start) / (float)CLOCKS_PER_SEC;
+	
+	printf ("Your calculations took %.2lf seconds to run.\n", seconds );
+	free(jobs);
+    
+	return 0;
 }
